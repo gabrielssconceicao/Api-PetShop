7ba1e0e4c48381bea6b3020e23eb9086c779f5c6
Gabriel Conceição <gabrielc2019@outlook.com.br>
2024-06-29 10:32:03 -0300

    config sequelize

--- src/routes/index.js
+++
@@ -1,7 +0,0 @@
-const { Router } = require('express');
-const { fornecedoresRoutes } = require('./fornecedores');
-const router = Router();
-
-router.use('/api/fornecedores', fornecedoresRoutes);
-
-module.exports = router;
